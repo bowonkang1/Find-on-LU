799f6ebca10bd0e3d0ac89da0b924c101ad7d203
Bowon Kang <kangbowon@gangbowon-ui-noteubug.local>
2025-10-12 17:00:38 -0500

    Created Authform for both sign up and sign in and also connected to supabase for the authentication

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://oxvexipgjqdjweaepjbm.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94dmV4aXBnanFkandlYWVwamJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY0Mzk4MTcsImV4cCI6MjA3MjAxNTgxN30.zC13qFo7n-npHDTuA2OJ7-56z0kSwatcvjeJy4n-nww'
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL!
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY!
+
+/*if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Missing Supabase environment variables')
+}*/
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
